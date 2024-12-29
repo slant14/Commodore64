e0b70ffd1d2fe5776f2752d468a024de307baa70
Mahmoud <mahmood.naderi98@gmail.com>
2024-12-29 18:20:19 +0300

    Add cpu and mem

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+using Byte = unsigned char;
+using Word = unsigned short;
+
+using u32 = unsigned int;
+
+struct Mem {
+    static constexpr u32 MAX_MEM = 1024 * 64;
+    Byte Data[MAX_MEM];
+
+    void init() {
+        for (u32 i = 0; i < MAX_MEM; i++) {
+            Data[i] = 0;
+        }
+    }
+};
+
+struct CPU {
+    Word PC; // program counter
+    Word SP; // stack pointer
+             
+    Byte A, X, Y; // Register
+    
+    Byte C : 1; // Status flag
+    Byte Z : 1; // Status flag
+    Byte I : 1; // Status flag
+    Byte D : 1; // Status flag
+    Byte B : 1; // Status flag
+    Byte V : 1; // Status flag
+    Byte N : 1; // Status flag
+
+    void reset(Mem memory) {
+        PC = 0xFFFC;
+        SP = 0x0100;
+
+        A = X = Y = 0;
+        C = Z = I = D = B = V = N = 0;
+
+        memory.init();
+    }
+
+};
+
+int main() {
+    
+    Mem mem;
+    CPU cpu;
+    cpu.reset(mem);
+
+    return 0;
+}
